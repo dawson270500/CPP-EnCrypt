9e968cf04acf5d0c5299bb934912c8417bc2ef82
Bailey Dawson <dawson270500@gmail.com>
2023-03-17 23:49:31 +0000

    Add project files.

---
+++ CPP-EnCrypt.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <time.h> 
+#include <string>
+#include "FileHandle.h"
+
+const string version = "1.0";
+
+using namespace std;
+using namespace file_handler;
+
+/// <summary>
+/// Convert a string to lower case
+/// </summary>
+/// <param name="LowerCase">String to make lower case</param>
+/// <returns>A lower case string</returns>
+string StringToLower(string LowerCase) {
+    string ret = "";
+    for (int i = 0; i < LowerCase.length(); i++) {
+        ret += tolower(LowerCase[i]);
+    }
+    return ret;
+}
+
+/// <summary>
+/// A very basic encryption program 
+/// 
+/// CPP-EnCrypt.exe -f#<file> -p#<password> [-encrypt / -decrypt] {-o#<output file>}
+/// Output file is required when using decrypt mode
+/// </summary
+/// <returns>0 for success, 1 for failure</returns>
+int main(int argc, char* argv[])
+{
+    cout << "EnCrypt version " + version << endl;
+    string in_file = "";
+    string out_file = "";
+    bool enMode = NULL;
+    bool enModeSet = false;
+    string password = "";
+
+    for (int i = 1; i < argc; i++) {
+        string curArg = StringToLower(argv[i]);
+        if (curArg.substr(0, 3) == "-f#" && in_file == "") {
+            in_file = curArg.substr(curArg.find("#") + 1, curArg.length());
+        }
+        else if (curArg.substr(0, 3) == "-o#" && password == "") {
+            out_file = curArg.substr(curArg.find("#") + 1, curArg.length());
+        }
+        else if (curArg.substr(0, 3) == "-p#" && password == "") {
+            password = curArg.substr(curArg.find("#") + 1, curArg.length());
+        }
+        else if (curArg == "-encrypt" && !enModeSet) {
+            enMode = true;
+            enModeSet = true;
+        }
+        else if (curArg == "-decrypt" && !enModeSet) {
+            enMode = false;
+            enModeSet = true;
+        }
+        else {
+            cout << "Invalid arguments" << endl;
+            return 1;
+        }
+    }
+
+    if (in_file == "") {
+        cout << "Invalid arguments, missing input file" << endl;
+        return 1;
+    }
+    if (enModeSet == false) {
+        cout << "Invalid arguments, mode missing" << endl;
+        return 1;
+    }
+    if (password == "") {
+        cout << "Invalid arguments, mode missing" << endl;
+        return 1;
+    }
+    if (enMode && out_file != ""){
+        cout << "Invalid arguments, output is not requried during encrypt" << endl;
+        return 1;
+    }
+    if (!enMode && out_file == "") {
+        cout << "Invalid arguments, output missing" << endl;
+        return 1;
+    }
+
+    if(enMode && out_file == "")
+        out_file = in_file.substr(0, in_file.find_last_of(".")) + ".enc";
+
+    cout << "Output file is " << out_file << endl;
+
+    int salt = 0;
+    for (int i = 0; i < password.length(); i++) {
+        salt += password[i];
+    }
+    cout << "Password converted to salt" << endl;
+
+    FileHandle input = FileHandle(in_file, FileMode::read);
+    cout << input.fileLen << " bytes to proccess" << endl;
+    FileHandle output = FileHandle(out_file, FileMode::write);
+    time_t start = std::time(nullptr);
+    if (!input.open) {
+        cout << "Error with input file, see above" << endl;
+        return 1;
+    }
+    if (!output.open) {
+        cout << "Error with output file, see above" << endl;
+        return 1;
+    }
+    for (int i = 0; i < input.fileLen; i++) {
+        if (i % 4 == 0) {
+            cout << "Completed " << i << " bytes" << endl;
+        }
+        char out = 0;
+        if (!input.getbyte(&out)) {
+            if (input.CurFilePos == -1) { // If we hit end of file
+                break;
+            }
+
+            cout << "Failed to read from file at byte " << i << endl;
+            input.close();
+            output.close();
+            return 1;
+        }
+        if(enMode)
+            out = (int)out + salt;
+        else
+            out = (int)out - salt;
+        if (!output.writebyte(out)) {
+            cout << "Failed to write to file at byte " << i << endl;
+            input.close();
+            output.close();
+            return 1;
+        }
+    }
+    time_t end = time(0);
+    input.close();
+    output.close();
+    cout << "Successfully ran, took " << start - end << " seconds" << endl;
+    return 0;
+}
---
+++ FileHandle.h
@@ -0,0 +1,112 @@
+#pragma once
+#include <string>
+#include <fstream>
+using namespace std;
+
+// File handling header
+namespace file_handler {
+	/// <summary>
+	/// A file handling class
+	/// </summary>
+	class FileHandle
+	{
+	public:
+		/// <param name="path">Path to file</param>
+		/// <param name="readWriteMode">0 for write mode, 1 for read mode</param>
+		FileHandle(string path, int readWriteMode) {
+			if (readWriteMode == 1 && !file_exist(path)) { // Check if we can access the file
+				open = false;
+				cout << "File does not exist" << endl;
+				return;
+			}
+			try {
+				// Check what mode we are in
+				if (readWriteMode == 0) {
+					this->mode = readWriteMode;
+					this->file.open(path, ios_base::out | ios_base::trunc);
+				}
+				else {
+					this->mode = readWriteMode;
+					this->file.open(path, ios_base::in);
+				}
+				if (file.is_open())
+					open = true; // Set open var to whether we opened or not
+				else {
+					open = false;
+					cout << "Failed to open file" << endl;
+				}
+				file.seekp(0, ios::end);
+				this->fileLen = file.tellp();
+				file.seekp(0);
+			}
+			catch (exception ex) { // Exception
+				open = false;
+				cout << "Failed to open file with exception " << ex.what() << endl;
+				file.close();
+			}
+		};
+
+		/// <summary>
+		/// Read a single byte from a file
+		/// </summary>
+		/// <param name="out">Var to output the byte to</param>
+		/// <returns>True if success, false if failed</returns>
+		bool getbyte(char* out) {
+			if (this->mode == 0)
+				return false;
+			file.read((char*)out, sizeof(char));
+			this->CurFilePos = this->file.tellp(); // Allows us to track when we hit the end of the file
+			return file.good();
+		};
+
+		/// <summary>
+		/// Write a byte to a file
+		/// </summary>
+		/// <param name="write">Byte to write</param>
+		/// <returns>true if success, false if failed</returns>
+		bool writebyte(char write) {
+			if (this->mode == 1)
+				return false;
+			this->file.put(write);
+			this->file.flush();
+			return file.good();
+		};
+
+		/// <summary>
+		/// Close the file
+		/// </summary>
+		void close() { 
+			this->file.close();
+		};
+
+		bool open = false; // Whether the file is open
+		int fileLen = 0; // The total lenght of the file
+		int CurFilePos = 0; // Current file position
+
+		/// <summary>
+		/// Check if a file exists
+		/// </summary>
+		/// <param name="fileName">File to check for existance</param>
+		/// <returns>True if exists</returns>
+		static bool file_exist(string fileName)
+		{
+			std::ifstream infile(fileName);
+			bool out = infile.good();
+			infile.close();
+			return out;
+		}
+
+	private:
+		fstream file; // File holder
+		int mode = -1; // Read write mode tracker
+	};
+
+	/// <summary>
+	/// Enum for file handle mode
+	/// </summary>
+	enum FileMode
+	{
+		write = 0,
+		read = 1
+	};
+}
